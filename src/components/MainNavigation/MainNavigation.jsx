c7c2f951d9dcdbc898374872989d077a0c06755c
MuratYvs <muratyvs99@gmail.com>
2024-04-19 11:11:46 +0300

    Update main navigation component

--- src/components/MainNavigation/MainNavigation.jsx
+++ src/components/MainNavigation/MainNavigation.jsx
@@ -1,12 +1,12 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
 import styles from "./MainNavigation.module.css";
 import { NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { userActions } from "../../redux/user-slice";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { useState } from "react";
 
 const MainNavigation = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const { isLogin } = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
@@ -22,46 +22,82 @@ const MainNavigation = () => {
     dispatch(userActions.changeLoginState(true));
   }
 
+  const handleOpenMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <Navbar
-      collapseOnSelect
-      expand="lg"
-      className={`bg-body-tertiary ${styles["main-navigation"]}`}
-    >
-      <Container>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className={`me-auto ${styles["nav-links"]}`}>
-            <div className="d-flex">
-              <Nav.Link className={`${styles["nav-link-element"]}`}>
-                <NavLink className={`${styles.text}`} to="/">
-                  Home
-                </NavLink>
-              </Nav.Link>
-              <Nav.Link className={`${styles["nav-link-element"]}`}>
-                <NavLink className={`${styles.text}`} to="/storehouses">
-                  Storehouses
-                </NavLink>
-              </Nav.Link>
-            </div>
-            <div>
-              <Nav.Link className={` ${styles["nav-link-element"]}`}>
-                {isLogin === false ? (
-                  <NavLink className={`${styles.text}`} to="/auth">
-                    Login/Signup
-                  </NavLink>
-                ) : (
-                  <p className="m-0" onClick={() => handleLogout()}>
-                    Logout
-                  </p>
-                )}
-              </Nav.Link>
-            </div>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <nav className={`${styles["main-navigation"]}`}>
+      {isMenuOpen ? (
+        <ul className={`${styles["hamburger-menu"]}`}>
+          <NavLink className={`${styles.text}`} to="/">
+            Home
+          </NavLink>
+          <NavLink className={`${styles.text}`} to="/storehouses">
+            Storehouses
+          </NavLink>
+          {isLogin === false ? (
+            <NavLink className={`${styles.text}`} to="/auth">
+              Login/Signup
+            </NavLink>
+          ) : (
+            <p className="m-0" onClick={() => handleLogout()}>
+              Logout
+            </p>
+          )}
+        </ul>
+      ) : (
+        ""
+      )}
+      <div className={`${styles["nav-links"]}`}>
+        <NavLink className={`${styles.text}`} to="/">
+          Home
+        </NavLink>
+
+        <NavLink className={`${styles.text}`} to="/storehouses">
+          Storehouses
+        </NavLink>
+      </div>
+
+      <div className={` ${styles["nav-links"]}`}>
+        {isLogin === false ? (
+          <NavLink className={`${styles.text}`} to="/auth">
+            Login/Signup
+          </NavLink>
+        ) : (
+          <p className="m-0" onClick={() => handleLogout()}>
+            Logout
+          </p>
+        )}
+      </div>
+      <RxHamburgerMenu className={`${styles.icon}`} onClick={handleOpenMenu} />
+    </nav>
   );
 };
 
 export default MainNavigation;
+
+{
+  /* 
+  <NavLink className={`${styles.text}`} to="/">
+    Home
+  </NavLink>
+
+  <NavLink className={`${styles.text}`} to="/storehouses">
+    Storehouses
+  </NavLink>
+
+<div>
+<Nav.Link className={` ${styles["nav-link-element"]}`}>
+  {isLogin === false ? (
+    <NavLink className={`${styles.text}`} to="/auth">
+      Login/Signup
+    </NavLink>
+  ) : (
+    <p className="m-0" onClick={() => handleLogout()}>
+      Logout
+    </p>
+  )}
+</Nav.Link>
+</div> */
+}
